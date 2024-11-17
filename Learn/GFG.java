d533eac4c7c8282245fa8a00291f398db2a617f9
Siddharth1006 <siddharthsoma01@gmail.com>
2024-11-17 15:24:01 +0530

    Input an integer using BufferedReader and throws keyword.

--- Learn/GFG.java
+++ Learn/GFG.java
@@ -2,11 +2,13 @@ import java.io.*;
 
 public class GFG{
     public static void main(String[] args) throws IOException {
+        //throws is a keyword that is used to tell the compiler that this code can throw "IOException"
+        //whoever is using this code has to cast/handle this exception.
         BufferedReader BuffReader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("Enter a String: ");
-        String str = BuffReader.readLine();
+        System.out.println("Enter an integer");
+        int x = Integer.parseInt(BuffReader.readLine()); // Explicitly typecasting a stream of characters to an integer
 
-        System.out.println("You entered a String: " + str);
+        System.out.println("You entered: " + x);
     }
 }
