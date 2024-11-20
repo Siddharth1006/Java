84e92c63793276d4e89ea7f3b63b5af045e9d187
Siddharth1006 <siddharthsoma01@gmail.com>
2024-11-20 18:08:19 +0530

    Formatting using DecimalFormat class

--- Learn/GFG.java
+++ Learn/GFG.java
@@ -1,19 +1,41 @@
 import java.io.*;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class GFG{
     public static void main(String[] args) {
-        //using arguments
+        double num = 123.4567;
 
-        if (args.length > 0) {
-            //then lets iterate through all the values of the argument array
-            System.out.println("Printing all arguments");
-            for (String str : args) {
-                System.out.println(str);
-            }
-        } else {
-            System.out.println("Arguments array is empty");
-        }
+        // prints only numeric part of a floating number
+        DecimalFormat ft = new DecimalFormat("####");
+        System.out.println("Without fraction part: num = "
+                + ft.format(num));
+
+        // this will print it upto 2 decimal places
+        ft = new DecimalFormat("#.##");
+        System.out.println(
+                "Formatted to Give precision: num = "
+                        + ft.format(num));
+
+        // automatically appends zero to the rightmost part
+        // of decimal instead of #,we use digit 0
+        ft = new DecimalFormat("#.000000");
+        System.out.println(
+                "appended zeroes to right: num = "
+                        + ft.format(num));
+
+        // automatically appends zero to the leftmost of
+        // decimal number instead of #,we use digit 0
+        ft = new DecimalFormat("00000.00");
+        System.out.println(
+                "formatting Numeric part : num = "
+                        + ft.format(num));
+
+        // formatting money in dollars
+        double income = 23456.789;
+        ft = new DecimalFormat("$###,###.##");
+        System.out.println("your Formatted Dream Income : "
+                + ft.format(income));
     }
 }
 
