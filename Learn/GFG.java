53b55b6ce74619a94fe510360d28684b6c608330
Siddharth1006 <siddharthsoma01@gmail.com>
2024-11-20 20:41:28 +0530

    instance of operator

--- Learn/GFG.java
+++ Learn/GFG.java
@@ -6,20 +6,38 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class GFG{
-    public static void main(String[] args) throws ParseException  {
-        //Formatting date as per the given pattern in the argument
-        SimpleDateFormat ft = new SimpleDateFormat("dd-MM-yyyy");
-        String date = ft.format(new Date());
+    public static void main(String[] args) {
 
-        System.out.println("Formatted Date: " + date);
+        Person obj1 = new Person();
+        Person obj2 = new Boy();
 
-        String anotherDate = "23/11/2024";
-        ft = new SimpleDateFormat("MM/dd/yyyy");
-        Date parsedAnotherDate = ft.parse(anotherDate); // We need to handle exception here. So, adding it.
-        System.out.println("Another date in another format: " + parsedAnotherDate);
+        // As obj is of type person, it is not an
+        // instance of Boy or interface
+        System.out.println("obj1 instanceof Person: "
+                + (obj1 instanceof Person));
+        System.out.println("obj1 instanceof Boy: "
+                + (obj1 instanceof Boy));
+        System.out.println("obj1 instanceof MyInterface: "
+                + (obj1 instanceof MyInterface));
 
-        System.out.println("Parsed Date: " + parsedAnotherDate);
+        // Since obj2 is of type boy,
+        // whose parent class is person
+        // and it implements the interface Myinterface
+        // it is instance of all of these classes
+        System.out.println("obj2 instanceof Person: "
+                + (obj2 instanceof Person));
+        System.out.println("obj2 instanceof Boy: "
+                + (obj2 instanceof Boy));
+        System.out.println("obj2 instanceof MyInterface: "
+                + (obj2 instanceof MyInterface));
     }
 }
 
+class Person {
+}
 
+class Boy extends Person implements MyInterface {
+}
+
+interface MyInterface {
+}
