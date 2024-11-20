3187e0b0feb0bb5790bf0cebe34e04f3f0339a7c
Siddharth1006 <siddharthsoma01@gmail.com>
2024-11-20 16:59:29 +0530

    Doing the same using BufferedReader

--- Learn/GFG.java
+++ Learn/GFG.java
@@ -1,26 +1,22 @@
 import java.io.*;
 import java.util.Scanner;
 
-//Scanner is preferred instead of BufferedReader
+// To read from console using BufferedReader
 public class GFG{
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("Enter an integer");
-        //using nextInt() to parse Integer values
-        int a = sc.nextInt();
-        // SOLUTION
-        sc.nextLine();
-        System.out.println("Enter a string");
-        String str = sc.nextLine();
-        System.out.println("Enter a double value: ");
-        double doub = sc.nextDouble();
-        System.out.println("Enter a floating point value");
-        float flot = sc.nextFloat();
-        System.out.println("Enter a long value");
-        long longlongvalue = sc.nextLong();
 
-        System.out.format("All values : %d , %s , %3.2f , %04.2f , %dl" , a , str, doub ,flot , longlongvalue);
+        //Taking integer input
+        int a = Integer.parseInt(br.readLine());
+
+        System.out.println("Enter a string");
+
+        String b = br.readLine();
+
+        //Printing
+        System.out.println("Integer is: " + a + " and String is: " + b);
     }
 }
 
