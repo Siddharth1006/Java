948796924db5745cb1f7948d6760c2d4ef6c5a01
Siddharth1006 <siddharthsoma01@gmail.com>
2024-11-24 17:04:29 +0530

    isPrime ? - very Efficient - for LARGE numbers

--- Learn/GFG.java
+++ Learn/GFG.java
@@ -8,15 +8,20 @@ import java.util.Scanner;
 public class GFG{
     public static boolean SYSTEM_LOCAL = true;
 
-    public static boolean isPrime(int n) {
-        // Corner case
-        if (n <= 1)
+    static boolean isPrime(int n) {
+        if (n == 1)
             return false;
 
-        // Check from 2 to n-1
-        for (int i = 2; i*i < n; i++)
-            if (n % i == 0)
+        if (n == 2 || n == 3)
+            return true;
+
+        if (n%2 == 0 || n%3 == 0)
+            return false;
+
+        for (int i = 5 ; i*i <= n ; i += 6){
+            if(n % i == 0 || n % (i + 2) == 0)
                 return false;
+        }
 
         return true;
     }
