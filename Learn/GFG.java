4650ce28f3eed00046db9d31d27dc0e3c6148514
Siddharth1006 <siddharthsoma01@gmail.com>
2024-11-24 16:21:09 +0530

    GCD of 2 numbers - recursion

--- Learn/GFG.java
+++ Learn/GFG.java
@@ -1,22 +1,35 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.math.BigInteger;
 import java.util.Scanner;
 
 public class GFG{
+    public static int gcd (int a , int b) {
+        // if b=0, a is the GCD
+        if (b == 0)
+            return a;
+            // call the gcd() method recursively by
+            // replacing a with b and b with
+            // difference(a,b) as long as b != 0
+        else
+            return gcd(b, Math.abs(a - b));
+    }
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream(new File("Learn/input-file.txt")));
+        System.setOut(new PrintStream(new File("Learn/output-file.txt")));
+//        System.setErr(new PrintStream(new File("err_output-file.txt")));
 
-    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the first number: ");
-        int num1 = sc.nextInt();
-        System.out.print("Enter the second number: ");
-        int num2 = sc.nextInt();
-        int smaller = Math.min(num1, num2);
-        int gcd = 1;
-        for (int i = 1; i <= smaller; i++) {
-            if (num1 % i == 0 && num2 % i == 0) {
-                gcd = i;
-            }
+        int testCases = sc.nextInt();
+        while (testCases-- > 0) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            int ans = gcd(A, B);
+            System.out.println(ans);
         }
-        System.out.println("The GCD of " + num1 + " and " + num2 + " is " + gcd);
         sc.close();
     }
 }
