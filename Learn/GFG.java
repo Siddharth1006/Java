14311c4fe03b69bcb711d368e35371ea3d5061d7
Siddharth1006 <siddharthsoma01@gmail.com>
2024-11-24 16:40:54 +0530

    LCM of 2 numbers

--- Learn/GFG.java
+++ Learn/GFG.java
@@ -7,14 +7,20 @@ import java.util.Scanner;
 
 public class GFG{
     public static int gcd (int a , int b) {
-        // if b=0, a is the GCD
-        if (b == 0)
-            return a;
-            // call the gcd() method recursively by
-            // replacing a with b and b with
-            // difference(a,b) as long as b != 0
-        else
-            return gcd(b, Math.abs(a - b));
+        int smaller = Math.min(a, b);
+        int larger = Math.max(a, b);
+        int remainder = larger % smaller;
+        while (remainder != 0) {
+            larger = smaller;
+            smaller = remainder;
+            remainder = larger % smaller;
+        }
+        return smaller;
+    }
+    public static int lcm (int a , int b) {
+        //u x v = LCM(u, v) * GCD (u, v)
+
+        return (a * b)/gcd(a , b);
     }
     public static void main(String[] args) throws FileNotFoundException {
         System.setIn(new FileInputStream(new File("Learn/input-file.txt")));
@@ -27,7 +33,7 @@ public class GFG{
             int A = sc.nextInt();
             int B = sc.nextInt();
 
-            int ans = gcd(A, B);
+            int ans = lcm(A, B);
             System.out.println(ans);
         }
         sc.close();
