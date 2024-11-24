15f21992e41d9f34a3c99cbb342c6eb3fd98e691
Siddharth1006 <siddharthsoma01@gmail.com>
2024-11-24 23:25:44 +0530

    Find all PrimeFactors.

--- Learn/GFG.java
+++ Learn/GFG.java
@@ -7,22 +7,24 @@ import java.util.Scanner;
 
 public class GFG{
     public static boolean SYSTEM_LOCAL = true;
-    public static int fibonacci(int n) {
-        if (n == 0) {
-            return 0;  // Base case for 0th Fibonacci number
-        }
-        if (n == 1) {
-            return 1;  // Base case for 1st Fibonacci number
-        }
 
-        int a = 0, b = 1;  // F(0) = 0, F(1) = 1
-        for (int i = 2; i <= n; i++) {
-            int next = a + b;  // Next Fibonacci number is the sum of the last two
-            a = b;  // Update a to the last Fibonacci number
-            b = next;  // Update b to the current Fibonacci number
+    public static void findPrimeFactors(int n) {
+        while (n % 2 == 0) {
+            System.out.print(2 + " ");
+            n /= 2;
         }
-        return b;  // Return the nth Fibonacci number
+        for (int i = 3; i * i <= n; i += 2) {
+            while (n % i == 0) {
+                System.out.print(i + " ");
+                n /= i;
+            }
+        }
+        if (n > 2) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
     }
+
     public static void main(String[] args) throws FileNotFoundException {
         if (SYSTEM_LOCAL) {
             System.setIn(new FileInputStream(new File("Learn/input-file.txt")));
@@ -31,9 +33,9 @@ public class GFG{
         }
         Scanner sc = new Scanner(System.in);
         int testCases = sc.nextInt();
-        while (testCases-- > 0) {
-            int N = sc.nextInt();
-            System.out.println(fibonacci(N));
+        while(testCases-- > 0) {
+            int n = sc.nextInt();
+            findPrimeFactors(n);
         }
         sc.close();
     }
