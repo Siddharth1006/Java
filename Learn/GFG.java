0935d50bee194a16012b0af5e22f38dabbb2a140
Siddharth1006 <siddharthsoma01@gmail.com>
2024-11-24 16:48:43 +0530

    improvements in code.

--- Learn/GFG.java
+++ Learn/GFG.java
@@ -6,27 +6,23 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class GFG{
-    public static int gcd (int a , int b) {
-        int smaller = Math.min(a, b);
-        int larger = Math.max(a, b);
-        int remainder = larger % smaller;
-        while (remainder != 0) {
-            larger = smaller;
-            smaller = remainder;
-            remainder = larger % smaller;
-        }
-        return smaller;
-    }
-    public static int lcm (int a , int b) {
-        //u x v = LCM(u, v) * GCD (u, v)
+    public static boolean SYSTEM_LOCAL = true;
 
-        return (a * b)/gcd(a , b);
+    public static int lcm (int a , int b) {
+        int ans = Math.min(a, b);
+        while (true) {
+            if (ans % a == 0 && ans % b == 0){
+                return ans;
+            }
+            ans++;
+        }
     }
     public static void main(String[] args) throws FileNotFoundException {
-        System.setIn(new FileInputStream(new File("Learn/input-file.txt")));
-        System.setOut(new PrintStream(new File("Learn/output-file.txt")));
-//        System.setErr(new PrintStream(new File("err_output-file.txt")));
-
+        if (SYSTEM_LOCAL) {
+            System.setIn(new FileInputStream(new File("Learn/input-file.txt")));
+            System.setOut(new PrintStream(new File("Learn/output-file.txt")));
+            System.setErr(new PrintStream(new File("err_output-file.txt")));
+        }
         Scanner sc = new Scanner(System.in);
         int testCases = sc.nextInt();
         while (testCases-- > 0) {
