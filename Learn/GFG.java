375f3154764dab7f47ab8061bf60d4d9ca781702
Siddharth1006 <siddharthsoma01@gmail.com>
2024-11-24 11:32:45 +0530

    Print a right-angled triangle.

--- Learn/GFG.java
+++ Learn/GFG.java
@@ -9,16 +9,30 @@ class Main {
 // } Driver Code Ends
 //User function Template for Java
 
-    public static void triangleWall(int s){
+// User function Template for Java
 
-        //Write your code here
-        for (int i = 1 ; i <= s ; ++i) {
-            for (int j = 0 ; j < i ; ++j) {
-                System.out.print("* ");
+    public static void triangle(int s) {
+
+        // Write your code here
+        for (int i = 1; i <= s; i++) {
+            if (i == 1) {
+                // Print the top of the triangle
+                System.out.println("*");
+            } else if (i == s) {
+                // Print the base of the triangle
+                for (int j = 1; j <= s; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            } else {
+                // Print the intermediate rows
+                System.out.print("*"); // First star
+                for (int j = 1; j <= (i - 1) * 2 - 1; j++) {
+                    System.out.print(" "); // Space in between
+                }
+                System.out.println("*"); // Last star
             }
-            System.out.println();
         }
-
     }
 
 //{ Driver Code Starts.
@@ -29,7 +43,7 @@ class Main {
         int t = scn.nextInt();
         while(t-- > 0) {
             int s = scn.nextInt();
-            triangleWall(s);
+            triangle(s);
 
             System.out.println("~");
         }
